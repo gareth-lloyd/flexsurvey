eaa3cf5dd1e04ee9f7b72a3cb30b1f49a4518f79
Gareth Lloyd <glloyd@gmail.com>
2012-12-02 21:16:36 +0000

    make installable

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='flexsurvey',
     version='0.0.1',
     author=u'Gareth Lloyd',
     author_email='glloyd@gmail.com',
-    packages=['survey'],
+    packages=find_packages(),
     url='https://github.com/gareth-lloyd/flexsurvey',
     license='BSD licence, see LICENCE.txt',
     description='Create flexible surveys in Django',
     long_description=open('README.txt').read(),
+    include_package_data=True,
 )
